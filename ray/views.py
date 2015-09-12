9ecb02d5479a9c2cf4757ce90681f78396f74901
mrudu <lou.jarak@gmail.com>
2015-09-12 11:30:41 +0530

    Doctor ID in views filter

--- ray/views.py
+++ ray/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from ray.models import *
 
-def getScheduleBetween(s_date, e_date):
-    appointments_between = Appointment.objects.filter(scheduled_at__gt = s_date, scheduled_till__lt = e_date);
+def getScheduleBetween(s_date, e_date, doc_id):
+    appointments_between = Appointment.objects.filter(scheduled_at__gt = s_date, scheduled_till__lt = e_date, doctor=Doctor.objects.get(id=doc_id));
 
     return appointments_between
 
-def getScheduleDay(s_date):
-    return Appointment.objects.filter(scheduled_at__year = s_date.year, scheduled_at__month = s_date.month, scheduled_at__day = s_date.day)
+def getScheduleDay(s_date, doc_id):
+    return Appointment.objects.filter(scheduled_at__year = s_date.year, scheduled_at__month = s_date.month, scheduled_at__day = s_date.day, doctor=Doctor.objects.get(id=doc_id))
 
-def cancelAppointments(s_date, reason):
-    app = getScheduleDay(s_date).update(cancelled=True, cancelled_reason=reason)
+def cancelAppointments(s_date, reason, doc_id):
+    app = getScheduleDay(s_date).update(cancelled=True, cancelled_reason=reason, doctor=Doctor.objects.get(id=doc_id))
 
     return app
